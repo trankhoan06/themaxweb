81d4d5aa4aa89634b6a4be0f071dab7ef6ae74e5
trankhoan06 <155511953+trankhoan06@users.noreply.github.com>
2026-06-20 00:46:55 +0700

    update

--- asset/js/home.js
+++ asset/js/home.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
-    $('.header_menu_inner').click(function () {
-        $('.header_menu_inner').toggleClass('active');
-        $('.header_menu_nav').toggleClass('active');
+    // Initialize home clients tabs
+    let activeTab = $('.home_clients_tab_item.active').attr('data-tabs');
+    $('.home_clients_content_item').hide();
+    $('.home_clients_content_item[data-tabs="' + activeTab + '"]').css('display', 'flex');
+
+    // Handle tab clicks
+    $('.home_clients_tab_item').click(function () {
+        if ($(this).hasClass('active')) return;
+
+        $('.home_clients_tab_item').removeClass('active');
+        $(this).addClass('active');
+
+        let tabId = $(this).attr('data-tabs');
+
+        $('.home_clients_content_item').hide();
+        $('.home_clients_content_item[data-tabs="' + tabId + '"]').css('display', 'flex').hide().fadeIn(300);
     });
 });
---
+++ asset/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.header_menu_inner').click(function () {
+        $('.header_menu_inner').toggleClass('active');
+        $('.header_menu_nav').toggleClass('active');
+    });
+
+});
